da6d18e78bc24fd80d609a7a070fc8b3e199bed8
dhermys <96082998+dhermys@users.noreply.github.com>
2022-04-04 11:56:12 -0300

    Aula 1 parte 2

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -2,12 +2,7 @@ import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
     static numeroDeContas = 0;
-    agencia;
-    _cliente;
-
-    _saldo = 0;
-
-
+    
     set cliente(novoValor){
         if(novoValor instanceof Cliente){
             this._cliente = novoValor;
@@ -21,15 +16,17 @@ export class ContaCorrente{
     get saldo(){
         return this._saldo;
     }
-    
+
     constructor(agencia, cliente){
         this.agencia = agencia;
         this.cliente = cliente;
+        this._saldo = 0;
         ContaCorrente.numeroDeContas += 1;
     }
 
+
     sacar(valor){
-        if(this._saldo >= valor) {
+        if(this._saldo >= valor){
             this._saldo -= valor;
             return valor;
         }
@@ -39,13 +36,14 @@ export class ContaCorrente{
         if(valor <= 0)
         {
             return;
-        }
-        
-        this._saldo += valor;
+        } 
+        this._saldo += valor;           
     }
 
-    transferir(valor, conta){
+    tranferir(valor, conta){
+        
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
+        
     }
 }
